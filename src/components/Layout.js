f0ebe5107a7e6e4ccfb0525d5b61563f8a25517b
SifatSikder <bsse1221@iit.du.ac.bd>
2023-10-24 04:38:24 +0600

    updated

--- src/components/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Layout = ({ children, className = "" }) => {
-    return (
-        <div className={`${className} w-full h-full inline-block z-0 bg-light p-32  xl:!p-0 lg:!p-0 md:!p-0 sm:!p-0`}>{children}</div>
-    )
-}
-
-export default Layout
